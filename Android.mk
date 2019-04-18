9152b87f2df98fcd6a1b2329c4183cebee712fc4
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-04-18 12:20:13 -0400

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Prebuilt Java Libraries
+#
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.android.gms
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_SRC_FILES := libs/google-play-services.jar
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_SDK_VERSION := current
+include $(BUILD_PREBUILT)
