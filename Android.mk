371e8b83eb4a7a4b2464af9e1530076c08e822d1
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-04-22 16:13:39 -0400

    Add files via upload

---
+++ Android.mk
@@ -0,0 +1,12 @@
+#
+# Prebuilt Java Libraries
+#
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.android.gms
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_SRC_FILES := google-play-services.jar
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_SDK_VERSION := $(SUPPORT_CURRENT_SDK_VERSION)
+LOCAL_JAVA_LANGUAGE_VERSION := 1.7
+include $(BUILD_PREBUILT)
