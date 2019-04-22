f0dcd42eb8f7719a1dc4dba967f25742d840fc2d
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-04-22 16:15:16 -0400

    Updating some thing
    Updating some thing

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := com.google.android.gms
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_SRC_FILES := google-play-services.jar
+LOCAL_SRC_FILES := google-play-services/google-play-services.jar
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_SDK_VERSION := $(SUPPORT_CURRENT_SDK_VERSION)
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
