990030d810af80c919c19cbe18c0f9f8d447faac
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-04-24 23:20:46 -0400

    Fixing Some Files
    Fixing Some Files

--- Android.mk
+++ Android.mk
@@ -1,11 +1,11 @@
 #
-# Prebuilt Java Libraries
+# Prebuilt GMS Java Libraries
 #
 include $(CLEAR_VARS)
 LOCAL_MODULE := com.google.android.gms
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_SRC_FILES := libs/google-play-services.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := google-play-services.jar
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_SDK_VERSION := current
 include $(BUILD_PREBUILT)
