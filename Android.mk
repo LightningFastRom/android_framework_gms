8e196235979a420beac0320adc2fcd2b14b7468b
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-04-25 00:59:29 -0400

    Finily got it fixed

--- Android.mk
+++ Android.mk
@@ -1,11 +1,12 @@
 #
 # Prebuilt GMS Java Libraries
 #
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := com.google.android.gms
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := google-play-services.jar
+LOCAL_SRC_FILES := $(LOCAL_MODULE).jar
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_SDK_VERSION := current
 include $(BUILD_PREBUILT)
